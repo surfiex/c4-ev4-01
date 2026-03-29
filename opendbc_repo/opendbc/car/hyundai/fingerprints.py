c9f7226fffe772a989f90d12ed1e6804ca1ed47d
EV4 Port <ev4@comma.ai>
2026-03-29 09:25:44 +0900

    Fix KIA_EV4 fingerprint: remove EPS - car doesn't respond to 0x7d4 FW query

--- opendbc_repo/opendbc/car/hyundai/fingerprints.py
+++ opendbc_repo/opendbc/car/hyundai/fingerprints.py
@@ -1015,9 +1015,6 @@ FW_VERSIONS = {
     (Ecu.fwdCamera, 0x7c4, None): [
       b'\xf1\x00CT11.011.031.012551000HKP_CT125_50430099211EZ000',
     ],
-    (Ecu.eps, 0x7d4, None): [
-      b'\xf1\x00CT1 MDPS C 1.00 1.01 56300EZ000  2541_CT1kE_CAN101',
-    ],
   },
   CAR.KIA_EV6: {
     (Ecu.fwdRadar, 0x7d0, None): [
