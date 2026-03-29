ea0bb434d28467a45c89f8e173f118da9aaca4c7
EV4 Port <ev4@comma.ai>
2026-03-29 12:16:19 +0900

    Fix carcontroller lfa_block_msg bug

--- opendbc_repo/opendbc/car/hyundai/carstate.py
+++ opendbc_repo/opendbc/car/hyundai/carstate.py
@@ -291,7 +291,7 @@ class CarState(CarStateBase):
     self.buttons_counter = cp.vl[self.cruise_btns_msg_canfd]["COUNTER"]
     ret.accFaulted = cp.vl["TCS"]["ACCEnable"] != 0  # 0 ACC CONTROL ENABLED, 1-3 ACC CONTROL DISABLED
 
-    if self.CP.flags & HyundaiFlags.CANFD_LKA_STEERING:
+    if self.CP.flags & HyundaiFlags.CANFD_LKA_STEERING or self.CP.flags & HyundaiFlags.CANFD_LKA_STEERING_ALT:
       self.lfa_block_msg = copy.copy(cp_cam.vl["CAM_0x362"] if self.CP.flags & HyundaiFlags.CANFD_LKA_STEERING_ALT
                                           else cp_cam.vl["CAM_0x2a4"])
 
